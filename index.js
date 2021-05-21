e832998f25145bbcdd135c2dee284bac216c44ec
SaitoYoshiko <yo.saito3759@icloud.com>
2021-05-21 10:23:22 +0900

    今回は 調べる手間を省いてしまいました。

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 'use strict';
 const http = require('http');
+const fs = require('fs');
+const fileName = './index.html';
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
-    'Content-Type': 'text/plain; charset=utf-8'
+    'Content-Type': 'text/html; charset=utf-8'
   });
-  res.write(req.headers['user-agent']);
+  res.write(
+    '<!DOCTYPE html><html lang="ja"><h1>HTMLで一番大きな見出しになります</h1></html>'
+  );
   res.end();
 });
 const port = 8000;
